ef29770ec6ad856fa9c1743a24812a4e55c472c8
Ethanan1 <98491284+Ethanan1@users.noreply.github.com>
2022-08-09 21:50:24 -0700

    solved

--- assets/js/your-code.js
+++ assets/js/your-code.js
@@ -1,19 +1,65 @@
 export function getAllDogs() {
     // Your code here
+    const url = "/dogs"
+    const dogsData = await fetch(url)
+    return dogsData
 }
 
 export function getDogNumberTwo() {
     // Your code here
+    const url = "/dogs/2"
+    const singleDogData = fetch(url)
+    return singleDogData
 }
 
-export function postNewDog() {
+export async function postNewDog() {
     // Your code here
+    const url = "/dogs"
+    const paramsBody = new URLSearchParams({
+        name: "dogName",
+        age: 2
+    })
+    const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-wwww-form-urlencoded"
+        },
+        body: paramsBody
+    }
+
+    const postDataResponse = await fetch(url, options)
+    return postDataResponse
 }
 
-export function postNewDogV2(name, age) {
+export async function postNewDogV2(name, age) {
      // Your code here
+     const url = "/dogs"
+     const paramsBody = new URLSearchParams({
+        name: name,
+        age: age,
+     })
+     const options = {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/x-wwww-form-urlencoded"
+        },
+        body: paramsBody
+     }
+
+        const postDataResponse = await fetch(url, options)
+        return postDataResponse
 }
 
 export function deleteDog(id) {
       // Your code here
+      const url = `/dogs/${id}/delete`;
+      const options = {
+        method: "POST",
+        headers: {
+            AUTH: "ckyut5wau0000jyv5bsrud90y"
+        }
+    }
+
+        const dataFromServer = await fetch(url, options)
+        return dataFromServer;
 }
